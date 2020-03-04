1646c46075504e239ae9b56262465781cefae9cd
vipinmankar <43236007+vipinmankar@users.noreply.github.com>
2020-03-04 11:00:25 +0530

    Update BTree.cpp

--- BTree.cpp
+++ BTree.cpp
@@ -1,88 +1,103 @@
 
 
-//B tree cpp program
+// cpp program of bTree  ton insert and traverse the tree ,BTree is tree data structure which use to
+ //indexing of database.
+
 #include <iostream>
 using namespace std;
-class btnode{
+class create_tree_node{
     public :
-    int *value;
-    int t;
-    btnode **child;
-    int n;
+    int *Key;
+    int no_of_key;
+    create_tree_node **child;
+    int count_of_keys;
     bool leaf;
     
-    btnode(int t1,bool leaf1){
-        t=t1;
+    create_tree_node(int t1,bool leaf1){
+        no_of_key=t1;
         leaf=leaf1;
-        value=new int[2*t-1];
-        child=new btnode *[2*t];
-        n=0;
+        Key=new int[2*no_of_key-1];
+        child=new create_tree_node *[2*no_of_key];
+        count_of_keys=0;
     }
-    void split(int i,btnode *y){
-        btnode *z=new btnode(y->t,y->leaf);
-        z->n=t-1;
-        for (int j = 0; j < t-1; j++)
-            z->value[j] = y->value[j+t];
+    //Function to split note and creayte new node
+    void split(int i,create_tree_node *y){
+        create_tree_node *z=new create_tree_node(y->no_of_key,y->leaf);   // create a new node pointer
+        z->count_of_keys=no_of_key-1;   //new node t-1 node
+       
+        // Copy the last (t-1) keys of y to z
+        for (int j = 0; j < no_of_key-1; j++)
+            z->Key[j] = y->Key[j+no_of_key];
         
-        
+         // Copy the last t children of y to z
         if (y->leaf == false)
         {
-            for (int j = 0; j < t; j++)
-                z->child[j] = y->child[j+t];
+            for (int j = 0; j < no_of_key; j++)
+                z->child[j] = y->child[j+no_of_key];
         }
         
         
-        y->n = t - 1;
+        y->count_of_keys = no_of_key - 1;
         
-        
-        for (int j = n; j >= i+1; j--)
+        //create a space of new child in node
+        for (int j = count_of_keys; j >= i+1; j--)
             child[j+1] = child[j];
         
-        child[i+1] = z;
+        child[i+1] = z;  //link node with new child
         
-        for (int j = n-1; j >= i; j--)
-            value[j+1] = value[j];
+        // A key of y will move to this node. Find the location of
+        // new key and move all greater keys one space ahead
+        for (int j = count_of_keys-1; j >= i; j--)
+            Key[j+1] = Key[j];
         
-        value[i] = y->value[t-1];
+        Key[i] = y->Key[no_of_key-1];
         
-        n = n + 1;  }
+        count_of_keys = count_of_keys + 1;// inc the count of key
+        
+    }
+    
+    // function to insert a key to node if the node is not full
     void insertnonfull(int k){
-        int i = n-1;
+         // Initialize index as index of rightmost element
+        int i = count_of_keys-1;
         
+        // If this is a leaf node
         if (leaf == true)
         {
-            while (i >= 0 && value[i] > k)
+           // find location of new keys in node move all key greater than key to be insert in one side
+            while (i >= 0 && Key[i] > k)
             {
-                value[i+1] = value[i];
+                Key[i+1] = Key[i];
                 i--;
             }
             
-            value[i+1] = k;
-            n = n+1;
+            Key[i+1] = k;
+            count_of_keys = count_of_keys+1; // incementing values of key
         }
-        else
+        else   // If this node is not leaf
         {
-            while (i >= 0 && value[i] > k)
+            // Find the child which is going to have the new key
+            while (i >= 0 && Key[i] > k)
                 i--;
             
-            if (child[i+1]->n == 2*t-1)
+            if (child[i+1]->count_of_keys == 2*no_of_key-1)
             {
-                split(i+1, child[i+1]);
+                split(i+1, child[i+1]);   // If the child is full, then split it
                 
-                if (value[i+1] < k)
+                if (Key[i+1] < k)
                     i++;
             }
-            
+            child[i+1]->insertnonfull( k);
         }
         
     }
     void traverse(){
         int i;
-        for (i = 0; i < n; i++)
+        for (i = 0; i < count_of_keys; i++)
         {
             if (leaf == false)
                 child[i]->traverse();
-            cout <<"\n"<< value[i];
+            cout <<"\n"<< Key[i];
         }
         
         
@@ -92,31 +107,37 @@ class btnode{
     friend class btree;
 };
 class btree{
-public:
-    btnode  *root;
-    int t,temp;
-    btree(int t1){
-        root=NULL;
-        t=t1;
-    }
-    void traverse()
-    {  if (root != NULL) root->traverse(); }
-    void insert(){
-        cout<<" "<<"Enter the number to add"<<"\n" ;
-        cin>>temp;
-        if(root==NULL){
-            root=new btnode(t,true);
-            root->value[0]=temp;
-            root->n=1;
+    public:
+        create_tree_node  *root;   //pointer to root node
+        int t,temp;          //min degree
+        btree(int t1){       //contructor of class btree for initializes the root to null
+            root=NULL;
+            t=t1;
+        }
+        void traverse()         //tree traverse function
+        {
+            if (root != NULL) root->traverse();
+            
+        }
+        void insert(){      //first call to insert the data in to tree
+            cout<<" "<<"Enter the number to add"<<"\n" ;
+            cin>>temp;      //value to be insert to tree
+            if(root==NULL){     //if tree is empty
+                root=new create_tree_node(t,true);  //create a memory location for root
+                root->Key[0]=temp;       //assigning a key  to root node
+                root->count_of_keys=1;          // update a number of key in node
             
         }
         else{
-            if(root->n==(2*t-1)){
-                btnode *s=new btnode(t,false);
-                s->child[0]=root;
-                s->split(0,root);
+            if(root->count_of_keys==(2*t-1)){//check count of key in node is equal min degree the tree grown in height
+                create_tree_node *s=new create_tree_node(t,false);//Allocate memory location for new root
+                s->child[0]=root;   // make a old root to child of new root
+                s->split(0,root); //function to split old node by creating new node and move one 1 key in one node
                 int i=0;
-                if (s->value[0] < temp)
+                
+                
+                
+                if (s->Key[0] < temp)
                     i++;
                 s->child[i]->insertnonfull( temp);
                 root = s;
@@ -135,28 +156,29 @@ int main()
 {
     btree t(3);
     int opt;
-    while(1){
-        cout<<"Enter \n 1. insert \n 2.traverse \n3. Exit ";
+    while(1){ // infinte loop for inserting  or traverse or exit according to user choice
+        cout<<" \npress 1 for insert \npress 2 for traverse \npress 3 for Exit ";
         cin>>opt;
         switch(opt){
             case 1:
-                t.insert();
+                t.insert(); // function call to inset of Btree class
                 break;
             case 2:
-                t.traverse();
+                t.traverse(); //// function call to traverse of Btree class
                 break;
             case 3:
-                exit(0);
+                exit(0); // to exit from current working program
         }
     }
     return 0;
 }
-//Sample input :
-10 21 31 41 51 
-output 
-10 21 31 41 51
+/*
  //Sample input :
-70 31 31 21 51 
-output 
-21 21 31 51 70
-
+ 10 21 31 41 51
+ output
+ 10 21 31 41 51
+ //Sample input :
+ 70 31 31 21 51
+ output
+ 21 21 31 51 70
+ */
